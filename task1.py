c3ebe9f007152e99b630eef4a1f477c0877bbdb7
VedPrakashCGU <veddscipw@gmail.com>
2025-03-18 16:58:10 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+#Performing all mathematical operation
+a=int(input("Enter the first Number"))
+b=int(input("Enter the Second Numnber"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Add :",add)
+print("Sub :",sub)
+print("Mul :",mul)
+print("Div :",div)
---
+++ task2.py
@@ -0,0 +1,3 @@
+first=str(input("Enter the first Name : "))
+second=str(input("Enter the second Name : "))
+print(f"Hello , {first} {second}! Welcome to the Python Programe")
